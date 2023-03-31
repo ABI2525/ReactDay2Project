0a6cb88feb2d148e14a489aaa912706c6dfb7ae0
ABI2525 <727721euec004@skcet.ac.in>
2023-03-31 15:31:12 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {AppBar, Button,Tab, Tabs, Toolbar, Typography} from '@mui/material';
+import Shop2Icon from '@mui/icons-material/Shop2';
+import StoreIcon from '@mui/icons-material/Store';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ContactsIcon from '@mui/icons-material/Contacts';
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+        <AppBar sx={{background:'orange'}}>
+            <Toolbar>
+                <Shop2Icon/>
+                <Typography>MySHOP</Typography>
+                <Tabs>
+                   <StoreIcon sx={{marginTop:'7px',left:'5px'}}/> 
+                    <Tab label="our selling products"/>
+                    <ThumbUpIcon sx={{marginTop:'7px'}}/>
+                    <Tab label="best rated products"/>
+                    <ContactsIcon sx={{marginTop:'7px'}}/>
+                    <Tab  label="Contact"/>
+           
+                </Tabs>
+                <Button  variant="OutLined" sx={{marginLeft:'auto'}}> LogIn</Button>
+                <Button  variant="contained" sx={{marginLeft:'12px'}}> signUp</Button>
+            </Toolbar>
+        </AppBar>
+        
     </div>
-  );
+  )
 }
 
-export default App;
+export default Header;
